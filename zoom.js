d6e611bb788f142a80b7947cb36bf462bf483f86
wgnsy <84272225+wgnsy@users.noreply.github.com>
2022-01-04 16:32:07 +0100

    Fixes Zoom.js
    Small fixes for code consistency and optimization

--- zoom.js
+++ zoom.js
@@ -3,6 +3,8 @@ var Zoom = (function(){
     var _imgSrc;
     var _img;
     var _cursor;
+    var _clicked;
+    var _actualImg;
     const _zoomOptions = //zoom options
     {
         'width': '120px',
@@ -12,35 +14,55 @@ var Zoom = (function(){
     var init = function(){
         _img = $('.img-zoom'); 
         _cursor = $('.cursor');
-
         _imgSrc = _img.attr('src'); //get img src
-        _imgSrc = _imgSrc.replace("\\", "/"); 
+        _imgSrc = _imgSrc.replace("\\", "/");
+        _clicked = false; 
+        _actualImg = _img;
+        
         
         renderView(); // build divs
         getImgOffset();
         moveZoom();
     };
     var moveZoom = function(){
+        $(document).on('click','.cursor',function(event){
+            if(_clicked == false){
+                _cursor.removeClass('scale-on');
+                _cursor.addClass('scale-off');
+                _clicked = true;
+            }
+            
+        });
+        
+        
         $(document).on('mousemove',function(event){
             
-        
-            var x = event.clientX; // get X offset
-            var y = event.clientY; //get y offset
+        if(_clicked == false){
+            _img = $('.img-zoom');    
+            _imgSrc = _img.attr('src'); //get img src
+            if(_actualImg != _img){
+                changeImage();
+                _actualImg = _img;
+            }
+            
+            var x = event.pageX; // get X offset
+            var y = event.pageY; //get y offset
             var imgX = _imgCoords.x; // get img offset left
             var imgY = _imgCoords.y; // get img offset top
+            
+            
             
                 if(x >= imgX && x <= imgX+_img.width() //check if offset fit img width/height
                 &&
                    y >= imgY && y <= imgY+_img.height()){
-      
+                    
             _cursor.show(); //show magnifier
                 _cursor.removeClass('scale-off'); 
                 _cursor.addClass('scale-on'); //animate scale
-
-            _cursor.css('background-size',(_img.width() *_zoomOptions.zoom) + 'px ' + (_img.width() *_zoomOptions.zoom) + 'px'); //set magnifier zoom
+            _cursor.css('background-size',(_img.width() *_zoomOptions.zoom) + 'px ' + (_img.height() *_zoomOptions.zoom) + 'px'); //set magnifier zoom
             //calculate magnifier
             cx = (((x-_img.offset().left) * _zoomOptions.zoom) - (_cursor.width()/2));
-            cy = (((y-_img.offset().top) * _zoomOptions.zoom) - (_cursor.width()/2));
+            cy = (((y-_img.offset().top) * _zoomOptions.zoom) - (_cursor.height()/2));
             //set position
             _cursor.css({
                 "background-position": "-" + cx + 'px '+ "-" + cy + 'px'
@@ -53,6 +75,8 @@ var Zoom = (function(){
                     _cursor.removeClass('scale-on');
                     _cursor.addClass('scale-off');
                 }
+
+            }
             });
     };
     var getImgOffset = function() {
@@ -63,6 +87,9 @@ var Zoom = (function(){
         _imgCoords = coords;
         
       };
+      var changeImage = function(){
+          _cursor.css('background-image','url("' + _imgSrc + '")');
+      }
       var renderView = function(){
 
         if(_cursor.length) _cursor.remove();
@@ -72,7 +99,7 @@ var Zoom = (function(){
               'display':'none',
               'transform':'scale(0)',
               'border-radius':'50%',
-              'border':'3px solid green',
+              'border':'3px solid red',
               'position':'absolute',
               //'cursor':'none',
               'background-image':"url('" + _imgSrc + "')",
